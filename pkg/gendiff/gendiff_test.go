c236e1d187d08bbdd680692e9801b7711d9096ab
Ivan Bir <ivbir.job@gmail.com>
2026-05-18 10:45:16 +0000

    Add work with yaml

--- pkg/gendiff/gendiff_test.go
+++ pkg/gendiff/gendiff_test.go
@@ -1,26 +1,26 @@
-package gendiff
+package gendiff_test
 
 import (
-	"path/filepath"
 	"testing"
 
+	"code/pkg/gendiff"
 	"code/pkg/parser"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGenDiff(t *testing.T) {
-	basePath := filepath.Join("testdata", "fixture")
-	file1 := filepath.Join(basePath, "file1.json")
-	file2 := filepath.Join(basePath, "file2.json")
+func TestGenDiffJSON(t *testing.T) {
+	// Файлы JSON
+	file1 := "pkg/gendiff/testdata/fixture/file1.json"
+	file2 := "pkg/gendiff/testdata/fixture/file2.json"
 
 	data1, err := parser.ReadFile(file1)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	data2, err := parser.ReadFile(file2)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
+
+	diff := gendiff.GenDiff(data1, data2)
 
 	expected := `{
   - follow: false
@@ -31,8 +31,30 @@ func TestGenDiff(t *testing.T) {
   + verbose: true
 }`
 
-	result := GenDiff(data1, data2)
-	if result != expected {
-		t.Errorf("expected:\n%s\ngot:\n%s", expected, result)
-	}
+	assert.Equal(t, expected, diff)
+}
+
+func TestGenDiffYAML(t *testing.T) {
+	// Файлы YAML
+	file1 := "pkg/gendiff/testdata/fixture/file1.yml"
+	file2 := "pkg/gendiff/testdata/fixture/file2.yml"
+
+	data1, err := parser.ReadFile(file1)
+	assert.NoError(t, err)
+
+	data2, err := parser.ReadFile(file2)
+	assert.NoError(t, err)
+
+	diff := gendiff.GenDiff(data1, data2)
+
+	expected := `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`
+
+	assert.Equal(t, expected, diff)
 }
--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 // ReadFile читает файл по пути и возвращает map[string]interface{}
@@ -19,7 +22,7 @@ func ReadFile(path string) (map[string]interface{}, error) {
 		return nil, fmt.Errorf("cannot read file: %w", err)
 	}
 
-	ext := filepath.Ext(path)
+	ext := strings.ToLower(filepath.Ext(path))
 	switch ext {
 	case ".json":
 		var result map[string]interface{}
@@ -27,6 +30,14 @@ func ReadFile(path string) (map[string]interface{}, error) {
 			return nil, fmt.Errorf("cannot parse JSON: %w", err)
 		}
 		return result, nil
+
+	case ".yml", ".yaml":
+		var result map[string]interface{}
+		if err := yaml.Unmarshal(data, &result); err != nil {
+			return nil, fmt.Errorf("cannot parse YAML: %w", err)
+		}
+		return result, nil
+
 	default:
 		return nil, fmt.Errorf("unsupported file extension: %s", ext)
 	}
